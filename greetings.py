77471d308a01b0ff9acd77ae23d080f3ef3e94f4
Radmir96 <ulyabaev2018@gmail.com>
2025-01-23 05:41:47 +0300

    added output of values ​​to console

--- greetings.py
+++ greetings.py
@@ -4,3 +4,10 @@ content ='Выполнить домашнее задание 1'
 status = 'Сделано'
 created_date = '22-01-2025'
 issue_date = '23-01-2025'
+
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
