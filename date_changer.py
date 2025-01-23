c248386a0412df065d562ac156fbe47b913e934b
Radmir96 <ulyabaev2018@gmail.com>
2025-01-23 06:34:17 +0300

    added file with modified date output

---
+++ date_changer.py
@@ -0,0 +1,13 @@
+username = 'Радмир'
+title = 'Модуль 1'
+content ='Выполнить домашнее задание 1'
+status = 'Сделано'
+created_date = '22-01-2025'
+issue_date = '23-01-2025'
+
+print(f"Имя пользователя: {username}.")
+print(f"Заголовок заметки: {title}.")
+print(f"Описание заметки: {content}.")
+print(f"Статус заметки: {status}.")
+print(f"Дата создания заметки: {created_date[:5]}.")
+print(f"Дата истечения заметки: {issue_date[:5]}.")
