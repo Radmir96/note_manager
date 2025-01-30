483c9ff2bc279a63f604ff2181df2b1efa1f7b80
Radmir96 <ulyabaev2018@gmail.com>
2025-01-30 06:37:08 +0300

    Grade 1. Stage 2: Task 2

---
+++ update_status.py
@@ -0,0 +1,25 @@
+def valid_input_status(status, options):
+    '''
+    Забирает текущий статус выполнения заметки status и сравнивает с корректными
+    вариантами заметок options. Возвращает True если заметка есть в списке,
+    False если заметки нет в списке.
+    '''
+    if status in options:
+        return True
+    else:
+        return False
+status_options = ['выполнено', 'в процессе', 'отложено']
+# Есть словарь с данными о заметке пользователя
+note = {'username': 'Радмир',
+        'content': 'ДЗ по второму модулю',
+        'status': 'в процессе',
+        'created_date': '29-01-2025',
+        'issue_date': '31-01-2025',
+        'title_list': ['Grade1', 'Этап2', 'Задание2']}
+
+#Сохраняем текущий статус пользователя в переменную
+status = note['status']
+
+print(f"Текущий статус заметки:{status}.")
+
+status = input()
